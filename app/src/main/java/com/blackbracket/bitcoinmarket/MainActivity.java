d5e8c39571dd1eae7aa3d6287456d66deef95303
anish <anish.vahora5@gmail.com>
2017-09-11 18:07:11 +0530

    jst calculator remaining

--- app/src/main/java/com/blackbracket/bitcoinmarket/MainActivity.java
+++ app/src/main/java/com/blackbracket/bitcoinmarket/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         fabSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(AppConstants.BlackBracketBlog));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(AppConstants.   BlackBracketBlog));
                 startActivity(browserIntent);
             }
         });
